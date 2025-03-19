5cee07f3a4fedda35d19eb570a04e7727183588e
Felipe Santanna <felipe.santanna@umusic.com>
2025-03-19 17:31:15 -0300

    up

---
+++ src/pages/api/feed.js
@@ -0,0 +1,32 @@
+import { XMLParser } from "fast-xml-parser"
+export default async function handler(req, res) {
+    // Buscar o feed RSS
+      const response = await fetch("https://colunadofla.com/feed", {
+        next: { revalidate: 3600 }, // Revalidar a cada hora
+      })
+      
+      if (!response.ok) {
+        throw new Error(`Falha ao buscar o feed: ${response.status}`)
+      }
+  
+      const xml = await response.text();
+      const parser = new XMLParser({
+        ignoreAttributes: false,
+        attributeNamePrefix: "@_",
+      })
+  
+      // Parsear o XML
+      const result = parser.parse(xml);
+      const items = result.rss.channel.item;
+
+
+      const flamengoNews = items.map((item) => ({
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+        description: item.description,
+      }))
+
+      console.log(items);
+      return res.status(200).json(items);
+  }
